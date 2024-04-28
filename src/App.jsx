e424b91309f834b4ce6930ab45e5635b8102c017
Musfique55 <mdmusfiquepatwary@gmail.com>
2024-04-28 16:46:41 +0600

    completed banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-
+import banner1 from '../public/slide1_banner1.jpg'
+import banner2 from '../public/slide1_banner2.jpg'
 import { useEffect, useState } from 'react';
 import './App.css'
 import Craftcards from './Components/Craftcards';
+import Slider from './Components/Slider';
 
 function App() {
   const [items,setItems] = useState([]);
@@ -11,10 +13,21 @@ function App() {
     .then(data => setItems(data));
 },[])
 
+
   return (
-    <div className='mx-5 md:mx-12 lg:mx-20'>
+    <div >
+      {/* slider banner */}
+      <div className="grid grid-cols-1 gap-7 md:grid-cols-2 lg:grid-cols-3">
+          <div className="col-span-2 mb-12">
+          <Slider></Slider>
+          </div>
+          <div>
+            <img src={banner1} alt="" className='h-[315px] w-full object-cover mb-6'/>
+            <img src={banner2} alt="" className='h-[315px] w-full object-cover'/>
+          </div>
+      </div>
       {/* crafts card */}
-        <div>
+        <div className='mx-5 md:mx-12 lg:mx-20'>
           <h2 className='text-center font-bold text-4xl mb-8'>Craft Items</h2>
           <div className='grid gap-6 grid-col-1 md:grid-cols-2 lg:grid-cols-3'>
             {
---
+++ src/Components/Slider.jsx
@@ -0,0 +1,52 @@
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import '../index.css';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+const Slider = () => {
+    return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide id='slider1' className='flex flex-col items-start justify-start'>
+                <h2 className='font-semibold text-3xl mb-5'>Wooden vases</h2>
+                <p className='font-medium text-xl mb-5'>High quality 3d models set of wooden vases</p>
+                <button className='bg-black px-6 py-3 text-white text-base'>See More</button>
+        </SwiperSlide>
+        <SwiperSlide id='slider2'>
+                <h2 className='font-semibold text-3xl mb-5'>Decor vases</h2>
+                <p className='font-medium text-xl mb-5'>Table ornament/Living room art decor</p>
+                <button className='bg-black px-6 py-3 text-white text-base'>See More</button>
+        </SwiperSlide>
+        <SwiperSlide id='slider3'>
+                <h2 className='font-semibold text-3xl mb-5'>Wooden utensils</h2>
+                <p className='font-medium text-xl mb-5'>High quality 3d models set of wooden utensils</p>
+                <button className='bg-black px-6 py-3 text-white text-base'>See More</button>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+
+export default Slider;
